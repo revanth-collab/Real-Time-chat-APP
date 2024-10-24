727c511f6317f1794450c3c3e0b5c0f823dbd1e4
revanth-collab <gurramnagarevanth1@gmail.com>
2024-10-24 09:22:11 +0530

    Update server.js

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: 'http://localhost:3000', 
+        origin: 'https://real-time-chat-app-react-one.vercel.app/, 
         methods: ['GET', 'POST']
     }
 });
